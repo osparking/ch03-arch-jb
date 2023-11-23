a8a66b04ff51f29cbc846c4e62dd3fb6e8c3458a
osparking <jbpark03@gmail.com>
2023-11-23 15:23:34 +0900

    custom runner 뼈대 - CalculatorTest 용

---
+++ src/test/java/ju4_runner/CustomTestRunner.java
@@ -0,0 +1,21 @@
+package ju4_runner;
+
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+
+public class CustomTestRunner extends Runner {
+
+	@Override
+	public Description getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run(RunNotifier arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
