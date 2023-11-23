c6aa6b10dfcc5ab904dc8c2b9483b73394cd340c
osparking <jbpark03@gmail.com>
2023-11-23 20:46:45 +0900

    맞춤 시험 Runner 클래스 상세 코드

--- src/test/java/ju4_runner/CustomTestRunner.java
+++ src/test/java/ju4_runner/CustomTestRunner.java
@@ -1,21 +1,48 @@
 package ju4_runner;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.Test;
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class CustomTestRunner extends Runner {
+	
+	private Class<?> testedClass;
 
 	@Override
 	public Description getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return Description.createTestDescription(testedClass, 
+				this.getClass().getSimpleName() + " 설명");
 	}
 
 	@Override
-	public void run(RunNotifier arg0) {
-		// TODO Auto-generated method stub
-
+	public void run(RunNotifier notifier) {
+		System.out.println("Running tests with " + 
+				this.getClass().getSimpleName() + ": " + testedClass);
+		
+		try {
+			Object testObject = 
+					testedClass.getDeclaredConstructor().newInstance();
+			
+			for (Method method : testedClass.getMethods()) {
+				if (method.isAnnotationPresent(Test.class)) {
+					notifier.fireTestStarted(Description
+							.createTestDescription(testedClass, method.getName()));
+					method.invoke(testObject);
+					notifier.fireTestFinished(Description
+							.createTestDescription(testedClass, method.getName()));
+				}
+			}
+		} catch (InstantiationException | IllegalAccessException |
+				InvocationTargetException | IllegalArgumentException | 
+				NoSuchMethodException | SecurityException e) {
+			throw new RuntimeException(e);
+		}
 	}
-
 }
