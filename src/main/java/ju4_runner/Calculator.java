2129881fbacc9db1a1b0019fcb1da2649b59afd3
osparking <jbpark03@gmail.com>
2023-11-24 08:32:47 +0900

    두 메소드 추가 - 예외 투척 속성

--- src/main/java/ju4_runner/Calculator.java
+++ src/main/java/ju4_runner/Calculator.java
@@ -4,4 +4,18 @@ public class Calculator {
 	public double add(double num1, double num2) {
 		return num1 + num2;
 	}
+	
+	public double sqrt(double x) {
+		if (x < 0) {
+			throw new IllegalArgumentException("음수는 제곱근을 구할 수 없다.");
+		}
+		return Math.sqrt(x);
+	}
+	
+	public double div(double ed, double er) {
+		if (er == 0) {
+			throw new ArithmeticException("영으로 숫자를 나눌 수 없다.");
+		}
+		return ed/er;
+	}
 }
