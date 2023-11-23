5a47c1cb1377be9bed6719f26161134e4933e586
osparking <jbpark03@gmail.com>
2023-11-23 15:20:33 +0900

    junit 4 test method skeleton

---
+++ src/test/java/ju4_runner/CalculatorTest.java
@@ -0,0 +1,14 @@
+package ju4_runner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
