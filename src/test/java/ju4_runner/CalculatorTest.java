8fc56796703b437a52ed00d77b59e4441ba94bc0
osparking <jbpark03@gmail.com>
2023-11-23 21:00:57 +0900

    Calculator.add() 단위 시험

--- src/test/java/ju4_runner/CalculatorTest.java
+++ src/test/java/ju4_runner/CalculatorTest.java
@@ -3,12 +3,16 @@ package ju4_runner;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(CustomTestRunner.class)
 public class CalculatorTest {
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		Calculator calculator = new Calculator();
+		double result = calculator.add(10, 20);
+		assertEquals(result, result, 0);
 	}
 
 }
