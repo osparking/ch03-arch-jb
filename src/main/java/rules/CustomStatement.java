b839ea5d30ed72dcd6c05acf6c380a28527aaf86
osparking <jbpark03@gmail.com>
2023-11-25 09:24:27 +0900

    맞춤 Statement 골격 생성

---
+++ src/main/java/rules/CustomStatement.java
@@ -0,0 +1,13 @@
+package rules;
+
+import org.junit.runners.model.Statement;
+
+public class CustomStatement extends Statement {
+
+	@Override
+	public void evaluate() throws Throwable {
+		// TODO Auto-generated method stub
+
+	}
+
+}
