d43f6cfb91d7e795e9b993cdfb22aa79fce59c71
osparking <jbpark03@gmail.com>
2023-11-25 09:41:05 +0900

    맞춤 Statement 클래스 구현 완성

--- src/main/java/rules/CustomStatement.java
+++ src/main/java/rules/CustomStatement.java
@@ -1,13 +1,29 @@
 package rules;
 
+import java.io.PrintStream;
+
+import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
-public class CustomStatement extends Statement {
+import lombok.AllArgsConstructor;
+import space.jbpark.utility.MyUtil;
 
+@AllArgsConstructor
+public class CustomStatement extends Statement {
+	private Statement base;
+	private Description description;
+	private static final PrintStream ps = MyUtil.getPrintStream();
+	
 	@Override
 	public void evaluate() throws Throwable {
-		// TODO Auto-generated method stub
-
+		String name = this.getClass().getSimpleName() + 
+				" " + description.getMethodName();
+		ps.println(name + " 작업 개시" );
+		
+		try {
+			base.evaluate();
+		} finally {
+			ps.println(name + " 작업 종료");
+		}
 	}
-
 }
